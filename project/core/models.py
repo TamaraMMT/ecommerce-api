63f55df9b96c999f73032db6b7de5e6e276f838a
TamaraMMT <tamaramolinast@gmail.com>
2024-02-27 16:10:05 -0300

    added custom User model

--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+"""
+Models Databases
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class Manager(BaseUserManager):
+    """Manager user"""
+    def create_user(self, email, password=None, **others_fields):
+        if not email:  # Check for empty email
+            raise ValueError("The email cannot be empty")
+        user = self.model(email=self.normalize_email(email), **others_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create super user and return"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=366)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = Manager()
+
+    USERNAME_FIELD = 'email'
