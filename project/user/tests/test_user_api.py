a2072f1e8151e65e2cbf0116e6a66d6054802a63
TamaraMMT <tamaramolinast@gmail.com>
2024-02-29 21:04:27 -0300

    test user api

---
+++ project/user/tests/test_user_api.py
@@ -0,0 +1,105 @@
+"""
+Test for Users API
+"""
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+REGISTER_USER_URL = reverse('user:register')
+TOKEN_URL = reverse('user:token_obtain_pair')
+
+def create_user(**params):
+    """Create new user and return"""
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUser(TestCase):
+    """Test public user"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test create user success"""
+        data = {
+            'email': 'usertest@example.com',
+            'password': '123password',
+            'firstname': 'Firstame',
+            'lastname': 'Lastname',
+        }
+        response = self.client.post(REGISTER_USER_URL, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=data['email'])
+        self.assertTrue(user.check_password(data['password']))
+        self.assertNotIn('password', response.data)
+
+    def test_user_email_already_exists(self):
+        """Test returned error if email exists"""
+        data = {
+            'email': 'usertest@example.com',
+            'password': '123password',
+            'firstname': 'Firstame',
+            'lastname': 'Lastname',
+        }
+        create_user(**data)
+
+        response = self.client.post(REGISTER_USER_URL, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_password_too_short(self):
+        """Test if password is too short"""
+        data = {
+            'email': 'usertest@example.com',
+            'password': '123',
+            'firstname': 'Firstame',
+            'lastname': 'Lastname',
+        }
+        response = self.client.post(REGISTER_USER_URL, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        user_test = get_user_model().objects.filter(
+            email=data['email']
+        ).exists()
+        self.assertFalse(user_test)
+
+    def test_access_token_user(self):
+        """Test valid credentials for generate token access"""
+        test_user = {
+            'email': 'usertest@example.com',
+            'password': '123password',
+        }
+        create_user(**test_user)
+
+        data_user = {
+            'email': test_user['email'],
+            'password': test_user['password'],
+        }
+        response = self.client.post(TOKEN_URL, data_user)
+
+        self.assertIn('access', response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_access_token_user_unauthorized(self):
+        """Test unauthorized for invalid credentials"""
+        test_user = {
+            'email': 'usertest@example.com',
+            'password': '123password',
+        }
+        create_user(**test_user)
+        data_user = {
+            'email': 'usertestBAD@example.com',
+            'password': '123password'
+        }
+        response = self.client.post(TOKEN_URL, data_user)
+
+        self.assertNotIn('access', response.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
