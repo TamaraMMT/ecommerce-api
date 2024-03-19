6937a39798ad7a7b6e0ac7dfb4840ddf475a590d
TamaraMMT <tamaramolinast@gmail.com>
2024-03-19 13:36:34 -0300

    test return product name, and unique order per product

--- project/product/test/test_models.py
+++ project/product/test/test_models.py
@@ -1,5 +1,6 @@
 
 import pytest
+from django.db.utils import IntegrityError
 
 pytestmark = pytest.mark.django_db
 
@@ -11,8 +12,26 @@ class TestCategoryModel:
         assert category.__str__() == 'test_category'
 
 
+class TestProductModel:
+    def test_str_name_product(self, product_factory):
+        product = product_factory(name='product1')
+
+        assert product.__str__() == 'product1'
+
+
 class TestProductlineModel:
     def test_str_sku_output_(self, productline_factory):
         productline = productline_factory(sku='test_SKU_0')
 
         assert productline.__str__() == 'test_SKU_0'
+
+    def test_unique_order_productline_per_product(
+            self,
+            product_factory,
+            productline_factory
+    ):
+        product = product_factory()
+        productline_factory(product=product, order=1)
+
+        with pytest.raises(IntegrityError):
+            productline_factory(product=product, order=1)
