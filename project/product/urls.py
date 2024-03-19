3430b4b3b7b4f25955974eb84c7609c1a91f25cd
TamaraMMT <tamaramolinast@gmail.com>
2024-03-18 23:04:17 -0300

    added router.register CategoryViewSet

--- project/product/urls.py
+++ project/product/urls.py
@@ -1,4 +1,3 @@
-
 from rest_framework.routers import DefaultRouter
 
 from product.views import CategoryViewSet, ProductViewSet
@@ -6,12 +5,11 @@ from django.urls import path, include
 
 
 router = DefaultRouter()
-router.register(r"", ProductViewSet)
+router.register(r"products", ProductViewSet)  # Registro de las vistas de productos bajo el endpoint "products"
+router.register(r"categories", CategoryViewSet)  # Registro de las vistas de categorías bajo el endpoint "categories"
 
 app_name = 'product'
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('category', CategoryViewSet.as_view({'get': 'list'})),
-
+    path('', include(router.urls)),  # Incluye las URLs generadas por el enrutador
 ]
