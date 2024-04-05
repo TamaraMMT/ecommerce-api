5a12d89bb3e8b1089226f1437c1135719881ce52
TamaraMMT <tamaramolinast@gmail.com>
2024-04-05 00:45:06 -0400

    added test models

--- project/product/test/test_models.py
+++ project/product/test/test_models.py
@@ -1,6 +1,9 @@
 
 import pytest
-from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError, DataError
+
+from product.models import Category, Product, ProductLine
 
 pytestmark = pytest.mark.django_db
 
@@ -11,12 +14,110 @@ class TestCategoryModel:
 
         assert category.__str__() == 'test_category'
 
+    def test_category_max_length_name(self, category_factory):
+        name = "x" * 236
+        with pytest.raises(DataError):
+            category_factory(name=name)
+
+    def test_category_name_unique_field(self, category_factory):
+        category_factory(name="name_unique")
+        with pytest.raises(IntegrityError):
+            category_factory(name="name_unique")
+
+    def test_category_slug_max_length(self, category_factory):
+        slug = "x" * 256
+        with pytest.raises(DataError):
+            category_factory(slug=slug)
+
+    def test_category_slug_unique_field(self, category_factory):
+        category_factory(slug="slug_unique")
+        with pytest.raises(IntegrityError):
+            category_factory(slug="slug_unique")
+
+    def test_category_is_active_true_default(self, category_factory):
+        category = category_factory()
+        assert category.is_active is True
+
+    def test_parent_category_on_delete_protect(self, category_factory):
+        category1 = category_factory()
+        category_factory(parent=category1)
+        with pytest.raises(IntegrityError):
+            category1.delete()
+
+    def test_category_parent_field_null(self, category_factory):
+        category1 = category_factory()
+        assert category1.parent is None
+
+    def test_category_return_category_active_only_true(self, category_factory):
+        category_factory(is_active=True)
+        category_factory(is_active=False)
+        qs = Category.objects.active().count()
+        assert qs == 1
+
+    def test_category_return_category_active_only_false(self, category_factory):
+        category_factory(is_active=True)
+        category_factory(is_active=False)
+        qs = Category.objects.count()
+        assert qs == 2
 
 class TestProductModel:
-    def test_str_name_product(self, product_factory):
+    def test_product_str_name_product(self, product_factory):
         product = product_factory(name='product1')
-
         assert product.__str__() == 'product1'
+
+    def test_product_max_length_name(self, product_factory):
+        name = "x" * 101
+        with pytest.raises(DataError):
+            product_factory(name=name)
+
+    def test_product_slug_max_length(self, product_factory):
+        slug = "x" * 256
+        with pytest.raises(DataError):
+            product_factory(slug=slug)
+
+    def test_product_slug_unique_field(self, product_factory):
+        product_factory(slug="slug_unique")
+        with pytest.raises(IntegrityError):
+            product_factory(slug="slug_unique")
+
+    def test_product_pid_unique_field(self, product_factory):
+        product_factory(pid="u4s5d6f")
+        with pytest.raises(IntegrityError):
+            product_factory(pid="u4s5d6f")
+
+
+    def test_product_is_digital_false_default(self, product_factory):
+        obj = product_factory(is_digital=False)
+        assert obj.is_digital is False
+
+    def test_product_is_active_default_true(self, product_factory):
+        obj = product_factory(is_digital=True)
+        assert obj.is_active is True
+
+
+    def test_product_fk_category_on_delete_protect(self, category_factory, product_factory):
+        obj1 = category_factory()
+        product_factory(category=obj1)
+        with pytest.raises(IntegrityError):
+            obj1.delete()
+
+    def test_product_return_product_active_only_true(self, product_factory):
+        product_factory(is_active=True)
+        product_factory(is_active=False)
+        qs = Product.objects.active().count()
+        assert qs == 1
+
+    def test_product_return_active_only_false(self, product_factory):
+        product_factory(is_active=True)
+        product_factory(is_active=False)
+        qs = Product.objects.count()
+        assert qs == 2
+
+    def test_product_unique_together_name_slug(self, product_factory):
+        product_factory(name="Test Name", slug="Test Slug")
+
+        with pytest.raises(IntegrityError):
+            product_factory(name="Test Name", slug="Test Slug")
 
 
 class TestProductlineModel:
@@ -37,6 +138,17 @@ class TestProductlineModel:
         # Trying to create another Productline instance with the same order, should raise IntegrityError
         with pytest.raises(IntegrityError):
             productline_factory(product=product, order=1)
+
+    def test_productline_max_5_digits_price(self, productline_factory):
+        price = 123456
+        with pytest.raises(DataError):
+            productline_factory(price=price)
+
+    def test_productline_fk_product_on_delete_protect(self, product_factory, productline_factory):
+        product1 = product_factory()
+        productline_factory(product=product1)
+        with pytest.raises(IntegrityError):
+            product1.delete()
 
 
 class TestProductImageModel:
@@ -65,7 +177,6 @@ class TestAttributeTypeModel:
         assert obj.__str__() == "TestProductType-TestAttributeName"
 
 
-
 class TestProductlineAttributeValueModel:
     def test_unique_together(
             self,
@@ -77,7 +188,7 @@ class TestProductlineAttributeValueModel:
 
 
     ):
-        # Create instances of ProductType, AttributeType, AttributeValue, Product y ProductLine
+        # Create instances of ProductType, Attribute, Product and ProductLine
         product_type = product_type_factory()
         attr_type = attribute_factory(attribute_name="test_attribute", product_type=product_type)
         product = product_factory()
