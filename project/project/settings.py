9f27fd358d5d490f0376a489c09a9bb7620f6037
TamaraMMT <tamaramolinast@gmail.com>
2024-05-07 21:09:46 -0400

    minutes token lifetime

--- project/project/settings.py
+++ project/project/settings.py
@@ -206,7 +206,7 @@ CACHES = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "BLACKLIST_AFTER_ROTATION": False,
     "SIGNING_KEY": 'dfsdfsf',
